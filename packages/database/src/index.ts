79828523f249de06b9a1fbabd0b537a173958ea9
Isaac <dev.zacknmr28@gmail.com>
2026-07-13 17:34:12 -0600

    refactor(db): create Prisma client factory and re-export types

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,20 +1,21 @@
-import "dotenv/config";
-
 import { PrismaPg } from "@prisma/adapter-pg";
 
 import { PrismaClient } from "../generated/prisma/client.js";
 
-const connectionString = process.env.DATABASE_URL;
+export function createPrismaClient(
+  connectionString: string,
+): PrismaClient {
+  const adapter = new PrismaPg({
+    connectionString,
+  });
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not configured");
+  return new PrismaClient({
+    adapter,
+  });
 }
 
-const adapter = new PrismaPg({
-  connectionString,
-});
+export {
+  PrismaClient,
+} from "../generated/prisma/client.js";
 
-export const prisma = new PrismaClient({
-  adapter,
-});
-
+export * from "../generated/prisma/enums.js";
